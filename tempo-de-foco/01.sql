b5d7bea8abf63926eaf585ffe29e06e58399516d
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-12-24 19:13:17 -0300

    feat: primeiro tempo de foco

---
+++ tempo-de-foco/01.sql
@@ -0,0 +1,47 @@
+-- Lista de transações com apenas 1 ponto
+
+SELECT *
+FROM transacoes
+WHERE QtdePontos = 1;
+
+-- Lista de pedidos realizados no fim de semana
+SELECT IdTransacao,
+      DtCriacao,
+      strftime('%w',datetime(DtCriacao)) AS DtDiaDaSemana
+FROM transacoes
+WHERE DtDiaDaSemana
+IN ('0', '6');
+
+-- Lista de clientes com 0 pontos
+SELECT *
+FROM clientes
+WHERE qtdePontos = 0;
+
+-- Lista de clientes com 100 a 200 pontos (inclusive ambos)
+
+SELECT *
+FROM clientes
+WHERE qtdePontos >= 100 AND qtdePontos <= 200;
+
+-- Lista de produtos com nome que começa com "Venda de"
+SELECT *
+FROM produtos
+WHERE DescNomeProduto
+LIKE 'Venda de%';
+
+-- Lista de produtos com nome que termina com "Lover"
+SELECT *
+FROM produtos
+WHERE DescNomeProduto
+LIKE '%Lover';
+
+-- Lista de produtos que são "chapéu"
+SELECT *
+FROM produtos
+WHERE DescNomeProduto
+LIKE '%chapéu%';
+
+-- Lista de transações com o produto "Resgatar Ponei"
+SELECT *
+FROM transacao_produto
+WHERE IdProduto = 15
