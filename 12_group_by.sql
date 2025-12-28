0990a3f6c59b65f7d7f427fba1622cd9cff682af
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-12-28 12:08:00 -0300

    feat: consulta com GROUP BY

---
+++ 12_group_by.sql
@@ -0,0 +1,29 @@
+SELECT IdProduto, 
+       count(*)
+
+FROM transacao_produto
+
+GROUP BY IdProduto;
+
+SELECT 
+  strftime('%d',datetime(DtCriacao)) AS data_criacao,
+  count(idCliente) AS clientes_criados
+FROM transacoes
+
+GROUP BY data_criacao
+
+ORDER BY clientes_criados DESC;
+
+SELECT IdCliente AS id_cliente,
+       sum(qtdePontos) AS cliente_total_pontos,
+       count(IdTransacao) AS cliente_total_qtd_transacao
+FROM transacoes
+
+WHERE DtCriacao >= '2025-07-01'
+AND DtCriacao < '2025-08-01'
+
+GROUP BY IdCliente
+
+ORDER BY cliente_total_pontos DESC
+
+LIMIT 10;
